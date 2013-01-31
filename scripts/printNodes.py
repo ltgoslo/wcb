78baf8bfb27e78c15fd114ec3bca67f7dbfc49ff
Lars Jørgen Solberg <supersolberg@gmail.com>
2013-01-31 17:37:25 +0100

    Updatet printNodes.py to function with the rest of the codebase
    * Removed dead code
    * Replaced the now unnecessary parameter 'wikiconf' with a lookup in paths.txt
    * Fixed loading of mwlib.cdb

--- scripts/printNodes.py
+++ scripts/printNodes.py
@@ -6,42 +6,16 @@ from mwlib import wiki, uparser
 from mwlib.parser import nodes
 from mwlib import advtree, parser
 import argparse, codecs, sys
+import util, paths
 
-
-def printTree(article, indent=""):
-    if isinstance(article, nodes.Link):
-        text = ": " + article.target
-        if isinstance(article, nodes.ImageLink):
-            if article.isInline():
-                text = " (inline)" + text
-            else:
-                text = " (block)" + text
-    elif isinstance(article, nodes.Section):
-        text = ": (level " + str(article.level) + ") " + article.caption.strip()
-    elif isinstance(article, nodes.Paragraph):
-        text = ""
-    else:
-        text = ": " + article.caption.strip()
-    text = indent + article.__class__.__name__ + text
-    print text.encode("ascii", "backslashreplace")
-    for child in article:
-        printTree(child, "-" + indent)
-
-
-def printAdvTree(article, ident=""):
-    text = ident + article.__class__.__name__ + ":" + article.text
-    print text
-    for c in article.getAllChildren():
-        printAdvTree(c, "-" + ident)
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description='Prints the syntax tree of an article')
-    argparser.add_argument('wikiconf')
     argparser.add_argument('article', nargs='?', default=None, help="name of article")
     argparser.add_argument('--file', '-f', default=None, type=str, help="parse FILE")
     argparser.add_argument('--advanced', '-a', action='store_true', help="Convert to advtree")
+    args = argparser.parse_args()
 
-    args =  argparser.parse_args()
-    env = wiki.makewiki(args.wikiconf)
+    env = wiki.makewiki(paths.paths["wikiconf"])
 
     if args.file:
         try:
@@ -53,7 +27,7 @@ if __name__ == "__main__":
     elif args.article:
         tree = env.wiki.getParsedArticle(args.article)
     else:
-        parser.print_help()
+        argparser.print_help()
         exit(-1)
 
     if tree: 
